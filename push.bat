25ba34a8b99b51dd95f561befc8a78bd19e5f8d8
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 16:42:03 -0300

    fix: boton WhatsApp flotante en index, JS completo, visualizador mejorado

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "visualizador: paleta 16 colores, galeria inspiracion, sin disclaimer, JS completo"
+git commit -m "fix: boton WhatsApp flotante en index, JS completo, visualizador mejorado"
 git push
 pause
