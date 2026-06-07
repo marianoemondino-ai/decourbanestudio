81de14be60e3760045350fcff82e8387453ae590
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 16:28:23 -0300

    visualizador: paleta 16 colores, galeria inspiracion, sin disclaimer, JS completo

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: index.html truncado restaurado, titles SEO, H1 duplicado, JS limpio"
+git commit -m "visualizador: paleta 16 colores, galeria inspiracion, sin disclaimer, JS completo"
 git push
 pause
