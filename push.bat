8fc8518536a92759944eaa54b15c532f3d7b69e7
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 15:42:05 -0300

    fix: loader aislado en script propio, fondo blanco, logo 120px

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: loader fondo blanco, logo 120px desktop, remove preload fonts"
+git commit -m "fix: loader aislado en script propio, fondo blanco, logo 120px"
 git push
 pause
