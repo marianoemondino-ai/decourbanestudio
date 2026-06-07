4ae03c878b373826c00dc93539ebe77a11f7065d
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 16:05:55 -0300

    fix: index.html truncado restaurado, titles SEO, H1 duplicado, JS limpio

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: logo navbar 78px, loader logo 320px"
+git commit -m "fix: index.html truncado restaurado, titles SEO, H1 duplicado, JS limpio"
 git push
 pause
