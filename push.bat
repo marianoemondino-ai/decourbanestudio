1303b0e946432bce80e992e0d70d00715e14afce
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 16:48:29 -0300

    rebrand: Marcela reemplazada por DecoUrban Studio Staff en 20 paginas

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: boton WhatsApp flotante en index, JS completo, visualizador mejorado"
+git commit -m "rebrand: Marcela reemplazada por DecoUrban Studio Staff en 20 paginas"
 git push
 pause
