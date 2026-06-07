1dee2384c2543cb7d8bce844892246c55ad5b404
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 17:14:57 -0300

    fix: corregir imagenes duplicadas y erroneas en 10 paginas

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "rebrand: Marcela reemplazada por DecoUrban Studio Staff en 20 paginas"
+git commit -m "fix: corregir imagenes duplicadas y erroneas en 10 paginas"
 git push
 pause
