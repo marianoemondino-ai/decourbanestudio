37ce8340ecbe1bd3b42f114020f271608c58cfd5
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 15:44:19 -0300

    fix: logo navbar 78px, loader logo 320px

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: loader aislado en script propio, fondo blanco, logo 120px"
+git commit -m "fix: logo navbar 78px, loader logo 320px"
 git push
 pause
