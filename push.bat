bb02f571935da361c067456f6839b27e8838c344
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 15:29:58 -0300

    perf: fonts async, preconnect CDN, loader max 2s, logo original colors 96px

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: mobile nav all pages, image dedup, grammar voseo, sitemap fixed, privacidad mobile"
+git commit -m "perf: fonts async, preconnect CDN, loader max 2s, logo original colors 96px"
 git push
 pause
