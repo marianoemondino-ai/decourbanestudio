19ac25c75d10a7e94e8d7f90efb5e7857c9d5c34
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 18:02:20 -0300

    polish: fix img duplicates sitewide, producto.html main section + H1, blog rel-art thumbnails diversificados

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "redesign: hero video full-screen, estilo posse minimalista, fonts sistema, nav transparente"
+git commit -m "polish: fix img duplicates sitewide, producto.html main section + H1, blog rel-art thumbnails diversificados"
 git push
 pause
