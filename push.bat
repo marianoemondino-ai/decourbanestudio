1cc21d6e3c70003f894dad6804d5bdbc28941852
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 20:05:09 -0300

    fix: reemplazar 9 URLs CDN fabricadas, quitar video thumbnails de img tags, limpiar null bytes en 3 archivos

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd "C:\Users\User\decourban-web"
 if exist ".git\index.lock" del ".git\index.lock"
 if exist ".git\HEAD.lock" del ".git\HEAD.lock"
 git add -A
-git commit -m "fix: --gold #c4a46b, --bg-dark dark, CSS completo nosotros/contacto/exterior, breadcrumb coleccion, robots.txt"
+git commit -m "fix: reemplazar 9 URLs CDN fabricadas, quitar video thumbnails de img tags, limpiar null bytes en 3 archivos"
 git push
 pause
