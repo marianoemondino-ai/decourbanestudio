7e5b80b7adc3f81a32dd8dab7c4f40b7d63fc3db
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 23:16:04 -0300

    redesign: hero video full-screen, estilo posse minimalista, fonts sistema, nav transparente

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "redesign: nuevo estilo blanco minimalista, catalogo tienda, sin cotizador"
+git commit -m "redesign: hero video full-screen, estilo posse minimalista, fonts sistema, nav transparente"
 git push
 pause
