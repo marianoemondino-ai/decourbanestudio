9257056ecadb92b3d70e871c851e7d488f2a1c5d
Mariano Emondino <marianoemondino@gmail.com>
2026-06-07 22:45:35 -0300

    redesign: nuevo estilo blanco minimalista, catalogo tienda, sin cotizador

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "fix: corregir imagenes duplicadas y erroneas en 10 paginas"
+git commit -m "redesign: nuevo estilo blanco minimalista, catalogo tienda, sin cotizador"
 git push
 pause
