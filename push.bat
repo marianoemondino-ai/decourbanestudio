367fe4828e7f0b2b3ca45b5c941e7b5a0e4e5685
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 18:58:24 -0300

    fix: logo 64px nav 84px, quitar width height hardcodeado en 30 archivos, padding heroes, footer logo 52px

--- push.bat
+++ push.bat
@@ -1,6 +1,8 @@
 @echo off
 cd "C:\Users\User\decourban-web"
+if exist ".git\index.lock" del ".git\index.lock"
+if exist ".git\HEAD.lock" del ".git\HEAD.lock"
 git add -A
-git commit -m "fix: coleccion.html CSS completo (pgrid, pc, sidebar, toolbar, modals), logo mayor nav+footer, Mesa de centro Ratan anadida, contador 67 productos"
+git commit -m "fix: logo 64px nav 84px, quitar width height hardcodeado en 30 archivos, padding heroes, footer logo 52px"
 git push
 pause
