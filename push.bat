8c9c9cb77df3b7887ca674f04cf7bf5cc6e9c4f9
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 18:17:11 -0300

    polish: comparador nudge en 6 product pages, og:image en financiacion+visualizador, galleries en duette+gemini+roller, diversificar thumbnails blog, producto.html main section + H1

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\decourban-web"
 del /f /q ".git\index.lock" 2>nul
 del /f /q ".git\HEAD.lock" 2>nul
 git add -A
-git commit -m "polish: fix img duplicates sitewide, producto.html main section + H1, blog rel-art thumbnails diversificados"
+git commit -m "polish: comparador nudge en 6 product pages, og:image en financiacion+visualizador, galleries en duette+gemini+roller, diversificar thumbnails blog, producto.html main section + H1"
 git push
 pause
