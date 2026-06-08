244ff48c7a288beeb9e3e37f539f96b9332af66f
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 20:14:09 -0300

    feat: exterior section + AI images + SEO improvements

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd "C:\Users\User\decourban-web"
 if exist ".git\index.lock" del ".git\index.lock"
 if exist ".git\HEAD.lock" del ".git\HEAD.lock"
 git add -A
-git commit -m "fix: reemplazar 9 URLs CDN fabricadas, quitar video thumbnails de img tags, limpiar null bytes en 3 archivos"
+git commit -m "fix: CSS braces+duplicados, JS getElementById nav/mobNav en 17 archivos, BOM en 7 archivos, noopener, meta desc"
 git push
 pause
