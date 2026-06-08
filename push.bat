b127a48e65cc84aed23acd1debc9ae64f1f1133f
Mariano Emondino <marianoemondino@gmail.com>
2026-06-08 19:19:32 -0300

    fix: --gold #c4a46b, --bg-dark dark, CSS completo nosotros/contacto/exterior, breadcrumb coleccion, robots.txt

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ cd "C:\Users\User\decourban-web"
 if exist ".git\index.lock" del ".git\index.lock"
 if exist ".git\HEAD.lock" del ".git\HEAD.lock"
 git add -A
-git commit -m "fix: logo 64px nav 84px, quitar width height hardcodeado en 30 archivos, padding heroes, footer logo 52px"
+git commit -m "fix: --gold #c4a46b, --bg-dark dark, CSS completo nosotros/contacto/exterior, breadcrumb coleccion, robots.txt"
 git push
 pause
