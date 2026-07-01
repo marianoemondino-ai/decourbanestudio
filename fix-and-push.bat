adce2157e1074170b294c9ddc258494e7000fa5d
Mariano Emondino <marianoemondino@gmail.com>
2026-06-30 22:21:56 -0300

    fix: logo HD en nav de todas las paginas, 4 mesas removidas, aspect-ratio imagenes cuadradas

--- fix-and-push.bat
+++ fix-and-push.bat
@@ -2,24 +2,25 @@
 echo Pusheando cambios a Vercel...
 cd /d "%~dp0"
 
-:: Limpiar lock de git si existe
+:: Limpiar locks y posible corrupcion del indice git
 if exist ".git\index.lock" del /f ".git\index.lock"
+if exist ".git\MERGE_HEAD" del /f ".git\MERGE_HEAD"
 
 git config http.postBuffer 524288000
 git config http.lowSpeedLimit 0
 git config http.lowSpeedTime 999
 
-:: Agregar todos los cambios (HTML, CSS, imagenes, SVG)
+:: Agregar todos los cambios
 git add -A
 
-:: Mostrar estado antes de commitear
+:: Mostrar estado
 echo.
-echo --- Estado de los archivos ---
+echo --- Archivos a commitear ---
 git status --short
-echo ------------------------------
+echo ----------------------------
 echo.
 
-git commit -m "refactor: hunterdouglas.html reducido y logo SVG local"
+git commit -m "fix: logo HD en nav de todas las paginas, 4 mesas removidas, aspect-ratio imagenes cuadradas"
 
 git push origin main
 
